f5597d38f0eccb2fdd054623d7c28ce0ca5c8f3d
Andrea Censi <andrea@cds.caltech.edu>
2013-07-05 09:00:33 +0200

    minor

--- src/astatsa/expectation/interface.py
+++ src/astatsa/expectation/interface.py
@@ -1,15 +1,18 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod
 from contracts import contract, ContractsMeta
 
+
+__all__ = ['ExpectationInterface']
+
+
 class ExpectationInterface():
-    __metaclass__ = ContractsMeta
     
+    __metaclass__ = ContractsMeta
     
     @abstractmethod
     @contract(value='array', dt='float,>=0', returns='None')
     def update(self, value, dt=1.0):
         pass
-    
 
     @abstractmethod
     @contract(returns='array')
